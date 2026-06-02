6fedd5b8a2d5f3fa5511d5a42aca9d7cba124155
safiraqueen <safiraqueen@student.uns.ac.id>
2026-06-02 20:39:32 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,18 @@
 from abc import ABC, abstractmethod
 
-
 # TRISNIA - SRP
 class Hewan(ABC):
     def __init__(self, nama):
         self.nama = nama
-
-
     @abstractmethod
     def makan(self):
         pass
-
 
 # MADA - OCP
 # SAFIRA - ISP
 class HewanTerbang(Hewan):
     def terbang(self):
         print(f"{self.nama} sedang terbang.")
-
 
 # GISMA - LSP
 class Burung(HewanTerbang):
@@ -27,42 +22,29 @@ class Singa(Hewan):
     def makan(self):
         print(f"{self.nama} sedang makan.")
 
-
 # TRISNIA - SRP
 class Kandang:
     def __init__(self):
         self.hewan_list = []
-
-
     def tambah_hewan(self, hewan):
         self.hewan_list.append(hewan)
-
-
     def bersihkan_kandang(self):
         print("Kandang dibersihkan.")
-
 
 # AKBAR - DIP
 class KebunBinatang:
     def __init__(self, kandang):
         self.kandang = kandang
-
-
     def rawat_semua_hewan(self):
         for hewan in self.kandang.hewan_list:
             hewan.makan()
-
-
             if isinstance(hewan, HewanTerbang):
                 hewan.terbang()
-
-
+                
 kandang = Kandang()
 burung = Burung("Elang")
 singa = Singa("Simba")
-
 kandang.tambah_hewan(burung)
 kandang.tambah_hewan(singa)
-
 zoo = KebunBinatang(kandang)
-zoo.rawat_semua_hewan()
+zoo.rawat_semua_hewan()   
