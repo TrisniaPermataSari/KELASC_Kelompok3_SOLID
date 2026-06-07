47f17bad0ef0cacec3db9167511c3bde807a25e3
Trisnini <trisniapermata02@student.uns.ac.id>
2026-06-07 19:22:58 +0700

    Trisnia_Update Hewan.py

--- Hewan.py
+++ Hewan.py
@@ -1,1 +1,24 @@
+from abc import ABC, abstractmethod
 
+class Hewan(ABC):
+    def __init__(self, nama):
+        self.nama = nama
+
+    @abstractmethod
+    def makan(self):
+        pass
+
+
+class HewanTerbang(Hewan):
+    def terbang(self):
+        print(f"{self.nama} sedang terbang.")
+
+
+class Burung(HewanTerbang):
+    def makan(self):
+        print(f"{self.nama} sedang makan.")
+
+
+class Singa(Hewan):
+    def makan(self):
+        print(f"{self.nama} sedang makan.")
