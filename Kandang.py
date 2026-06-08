9045af92e69cee7a19b5c7214abd8ec2d90df70c
madamjee <fathmada@student.uns.ac.id>
2026-06-08 20:12:36 +0700

    update kandang.py - Mada

--- Kandang.py
+++ Kandang.py
@@ -1,1 +1,9 @@
+class Kandang:
+    def __init__(self):
+        self.hewan_list = []
 
+    def tambah_hewan(self, hewan):
+        self.hewan_list.append(hewan)
+
+    def bersihkan_kandang(self):
+        print("Kandang dibersihkan.")
